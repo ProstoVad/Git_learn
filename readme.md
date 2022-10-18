e3cfbc276bc00698a81f03d82571a8cdaaddb8e1
VADIM <62515524+ProstoVad@users.noreply.github.com>
2022-10-18 15:17:01 +0300

    added for list

--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 # Learn Git for Uni
-## It's second lab on OP on third semester
+### It's second lab on OP on third semester
 
 > In this lab I want to master Git
  
 
+ 
--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.body.innerHTML = `<p>git work</p>>`;
+document.getElementById('git').innerHTML = `<p>git work</p>`;
+
+let i = 1;
+for(i; i<10; i++){
+    document.getElementById('list').innerHTML = `<li>${i}</li>`;
+}
